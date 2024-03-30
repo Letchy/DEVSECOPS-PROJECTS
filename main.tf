a6d91790e9207637cdb6e48c4664ff18482ca5ab
brandonagoge <BKL83@Live.co.uk>
2024-03-30 22:41:49 +0000

    creation of new S3 bucket, named "terraform-up-and-running-state4-brandon"

--- main.tf
+++ main.tf
@@ -2,6 +2,11 @@ resource "aws_s3_bucket" "terraform-up-and-running-state3-brandon" {
   bucket = "terraform-up-and-running-state3-brandon"
 }
 
+resource "aws_s3_bucket" "terraform-up-and-running-state4-brandon" {
+  bucket = "terraform-up-and-running-state4-brandon"
+}
+
+
 resource "aws_instance" "test3" {
   ami           = "ami-0903ff18cc3c8e341" # Replace with the desired Amazon Linux 2 AMI for eu-west-2 (bitnami image)
   instance_type = "t2.micro"              # Change to the desired instance type
